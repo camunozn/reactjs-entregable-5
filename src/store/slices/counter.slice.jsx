7ab3c7f07618f639fc2b23799a942b6e708e5161
camunozn <camunozn89@gmail.com>
2023-01-17 23:15:39 -0500

    added counter slice

--- src/store/slices/counter.slice.jsx
+++ src/store/slices/counter.slice.jsx
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// Cambiamos mySlice por el nombre de nuestro slice (usersSlice, toDosSlice...)
 export const counterSlice = createSlice({
   name: "counterSlice",
   initialState: 10,
