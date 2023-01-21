fd3768b621a25723450a3ece6e7a66430dc685d7
camunozn <camunozn89@gmail.com>
2023-01-21 16:56:11 -0500

    added styles for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { HashRouter, Route, Routes } from 'react-router-dom';
-import InputName from './assets/components/InputName';
+import Home from './assets/components/Home';
 import Pokedex from './assets/components/Pokedex';
 import PokemonDetail from './assets/components/PokemonDetail';
 import ProtectedRoutes from './assets/components/ProtectedRoutes';
@@ -12,7 +12,7 @@ function App() {
     <HashRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<InputName />} />
+          <Route path="/" element={<Home />} />
           <Route element={<ProtectedRoutes />}>
             <Route path="/pokedex" element={<Pokedex />} />
             <Route path="/pokedex/:id" element={<PokemonDetail />} />
---
+++ src/assets/components/Home.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { changeUserName } from '../../store/slices/userName.slice';
+import homeImg from '../img/pokedex_title.png';
+
+const Home = () => {
+  const [user, setUser] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  return (
+    <div className="home container">
+      <section className="section-home">
+        <div className="home-title">
+          <img src={homeImg} alt="Pokedex title" />
+        </div>
+        <div className="home-welcome">
+          <h1 className="heading-primary">Hello trainer!</h1>
+          <p>Type your name to begin:</p>
+        </div>
+        <div className="home-input">
+          <input
+            placeholder="Type your name here"
+            type="text"
+            value={user}
+            onChange={e => setUser(e.target.value)}
+          />
+          <button
+            className="btn btn--input"
+            onClick={() => {
+              dispatch(changeUserName(user));
+              navigate('/pokedex');
+            }}
+          >
+            Let's Start
+          </button>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
