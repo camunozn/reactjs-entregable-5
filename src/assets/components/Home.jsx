6a65b95338e7f83a00ac0e2f7744ec30f48261b8
camunozn <camunozn89@gmail.com>
2023-01-25 16:01:58 -0500

    added pokedex component styles

--- src/assets/components/Home.jsx
+++ src/assets/components/Home.jsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { changeUserName } from '../../store/slices/userName.slice';
 import pokedexImg from '../img/pokedex_title.png';
+import Footer from './Footer';
 
 const Home = () => {
   const [user, setUser] = useState('');
@@ -11,33 +12,39 @@ const Home = () => {
 
   return (
     <div className="home container">
-      <section className="section-home">
-        <div className="home-title">
-          <img src={pokedexImg} alt="Pokedex title" />
-        </div>
-        <div className="home-welcome">
-          <h1 className="heading-primary">Hello trainer!</h1>
-          <p>Type your name to start the journey:</p>
-        </div>
-        <div className="home-input">
-          <input
-            className="input name-input"
-            placeholder="Type your name here"
-            type="text"
-            value={user}
-            onChange={e => setUser(e.target.value)}
-          />
-          <button
-            className="btn btn--input"
-            onClick={() => {
-              dispatch(changeUserName(user));
-              navigate('/pokedex');
-            }}
-          >
-            Let's begin
-          </button>
-        </div>
-      </section>
+      <header></header>
+      <main>
+        <section className="section-home">
+          <div className="home-title">
+            <img src={pokedexImg} alt="Pokedex title" />
+          </div>
+          <div className="home-welcome">
+            <h1 className="heading-primary">Hello trainer!</h1>
+            <p>Type your name to start the journey:</p>
+          </div>
+          <div className="home-input">
+            <input
+              className="input name-input"
+              placeholder="Type your name here"
+              type="text"
+              value={user}
+              onChange={e => setUser(e.target.value)}
+            />
+            <button
+              className="btn btn--input"
+              onClick={() => {
+                dispatch(changeUserName(user));
+                navigate('/pokedex');
+              }}
+            >
+              Let's begin
+            </button>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
